9dee95e6ae51d75fa2d06fd0080d44be7201ac1b
Christopher P Gray (cpgray) <cpgray@uwaterloo.ca>
2019-03-13 15:54:51 -0400

    reorganized into directories and insured testdoi could be run

--- xref/testdoi.py
+++ xref/testdoi.py
@@ -7,16 +7,15 @@ cr = Crossref()
 licenses = set([])
 assertions = set([])
 
-with open('2017-extract-chris.csv', 'rt',encoding='Windows-1252',newline='\n') as csvhdl:
+with open('data/2017-extract-chris.csv', 'rt',encoding='Windows-1252',newline='\n') as csvhdl:
     rdr = csv.DictReader(csvhdl)
     for r in rdr:
         doi = quote(r['DOI'])
         data = cr.works(ids=doi)
         msg = data['message']
         for lic in msg['license']:
-            
-        if 'assertion' in msg.keys():
-            for a in msg['assertion']:
-                if a['name'] == 'copyright':
-                    print(a['value'])
+            if 'assertion' in msg.keys():
+                for a in msg['assertion']:
+                    if a['name'] == 'copyright':
+                        print(a['value'])
 
