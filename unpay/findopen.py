75c001616e395e189880fa4d330dfb44cdeb9973
Christopher P Gray (cpgray) <cpgray@uwaterloo.ca>
2019-03-20 15:51:01 -0400

    modified findopen to use new directory structure

--- unpay/findopen.py
+++ unpay/findopen.py
@@ -5,13 +5,16 @@ from urllib.parse import quote
 
 with open('data/2017-extract-chris.csv', 'rt',encoding='Windows-1252',newline='\n') as csvhdl:
     rdr = csv.DictReader(csvhdl)
-    for r in rdr:
-        doi = quote(r['DOI'])
-        apiurl = 'https://api.unpaywall.org/v2/{0}?email=cpgrayca@uwaterloo.ca'.format(doi)
-        resp = urllib.request.urlopen(apiurl)
-        content = resp.read().decode('utf-8')
-        data = json.loads(content)
-        if data['is_oa']:
-            #print(json.dumps(data, indent=2))
-            print('{0},{1},{2},{3}'.format(data['is_oa'], data['oa_status'],
-                                       data['publisher'], doi))
+    with open ('unpay/details.csv', 'wt') as details:
+        fieldnames = ['doi', 'is_oa', 'best_oa_location/version',
+                      'journal_is_oa', 'journal_issns', 'journal_name',
+                      'oa_status', 'publisher']
+        wtr = csv.DictWriter(details, fieldnames=fieldnames)
+        for r in rdr:
+            doi = quote(r['DOI'])
+            apiurl = 'https://api.unpaywall.org/v2/{0}?email=cpgrayca@uwaterloo.ca'.format(doi)
+            resp = urllib.request.urlopen(apiurl)
+            content = resp.read().decode('utf-8')
+            data = json.loads(content)
+            if data['is_oa']:
+                
