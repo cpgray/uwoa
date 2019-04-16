75433a868128d4b9f763550f208dd4d658471bad
Christopher P Gray (cpgray) <cpgray@uwaterloo.ca>
2019-04-16 16:21:13 -0400

    did more work on cleaning up the sherpa/romeo list of apcs -- still need to handle per page and per quanta charges

--- sherp/sherpclean.py
+++ sherp/sherpclean.py
@@ -5,8 +5,20 @@ with open('sherp.csv') as sherp:
     with open('costs.csv', 'wt', newline='') as costs:
         wtr = csv.writer(costs)
         for r in rdr:
-            if r['Publisher'].strip().startswith('-'):
-                wtr.writerow([temp + ' ' + r['Publisher'], r['US Dollars']])
+            if r['US Dollars'].startswith('('):
+                price = r['US Dollars'][1:-1]
             else:
-                wtr.writerow([r['Publisher'], r['US Dollars']])
-                temp = r['Publisher']
+                price = r['US Dollars']
+            if '-' in price and price != '-':
+                price = price.split('-')
+                price = (int(price[0][1:]) + int(price[1][1:]))/2
+            elif price.startswith('$'):
+                price = price[1:]
+            if type(price) == type(1.0) or price.isdigit():
+                if r['Publisher'].strip().startswith('-'):
+                    wtr.writerow([temp + ' ' + r['Publisher'], price])
+                else:
+                    wtr.writerow([r['Publisher'], price])
+                    temp = r['Publisher']
+            else:
+                print('error', r)
