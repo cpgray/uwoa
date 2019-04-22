327a0d076274f58425eef2292168cb1205bf7c03
Christopher P Gray (cpgray) <cpgray@uwaterloo.ca>
2019-04-22 12:42:07 -0400

    completed script to get prices based on sherpa/romeo data

--- sherpprices.py
+++ sherpprices.py
@@ -23,29 +23,52 @@ with open('data/wos2.csv', 'rt') as wos2:
         wosdata.append(row)
 
 sherptitles = []
-pricelist = []
+pricelist = {}
 with open('sherp/costs.csv', 'rt') as costs:
     rdr = csv.DictReader(costs)
     for row in rdr:
         sherptitles.append(row['publisher'])
-        pricelist.append(row)
+        pricelist[row['publisher']] = row
 
 output = []
-for r in wosdata:
-    doi = quote(r['DOI'])
-    try:
-        resp = urllib.request.urlopen(apiurl.format(doi))
-    except urllib.error.HTTPError:
-        continue
-    data = resp.read().decode('utf-8')
-    jsondata = json.loads(data)
-    mp = process.extract(jsondata['publisher'], sherptitles,
-                         scorer=fuzz.token_sort_ratio,
-                         limit=1)
-    if mp[0][1] > 86:
-        print('pub', jsondata['publisher'], mp)
-    mj = process.extract(jsondata['journal_name'], sherptitles,
-                         scorer=fuzz.token_sort_ratio,
-                         limit=1)
-    if mj[0][1] > 86:
-        print('jou', jsondata['publisher'], mp)
+fieldnames = ['doi', 'apc', 'type', 'title']
+with open('sherpapcs.csv', 'wt') as apcs:
+    wtr = csv.DictWriter(apcs, fieldnames=fieldnames)
+    wtr.writeheader()
+    for r in wosdata:
+        doi = quote(r['DOI'])
+        try:
+            resp = urllib.request.urlopen(apiurl.format(doi))
+        except urllib.error.HTTPError:
+            continue
+        content = resp.read().decode('utf-8')
+        data = json.loads(content)
+        try:
+            mp = process.extract(data['publisher'], sherptitles,
+                                 scorer=fuzz.token_sort_ratio,
+                                 limit=1)
+            mj = process.extract(data['journal_name'], sherptitles,
+                                 scorer=fuzz.token_sort_ratio,
+                                 limit=1)
+        except TypeError:
+            print(doi, data['journal_name'], data['publisher'])
+            continue
+        matchon = []
+        if mp[0][1] > 86:
+            matchon.append('publisher')
+        if mj[0][1] > 86:
+            matchon.append('journal')
+        if len(matchon) == 0:
+            continue
+        if data['oa_status'] != 'hybrid' or r['Publication Date'] != '2017':
+            continue
+        article = {'doi': doi}
+        if 'publisher' in matchon:
+            article['title'] = mp[0][0]
+        if 'journal' in matchon:
+            article['title'] = mj[0][0]
+        article['apc'] = pricelist[article['title']]['price']
+        article['type'] = data['oa_status']
+        wtr.writerow(article)
+        apcs.flush()
+
