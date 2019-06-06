83791502d9cd4eb04f46f4970ea6a8e067e58df5
Christopher P Gray (cpgray) <cpgray@uwaterloo.ca>
2019-06-06 16:19:30 -0400

    clean up of WoS CSV files, removing summaries not part of the data and converting from Windows encoding to Unicode

---
+++ report/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import os
+from pprint import pprint
+
+## list of files from Shannon with WoS data
+files = ['OA 2013-2017.csv',
+         'OA 2018 WoS.csv',
+         'OA other 2013-2017.csv',
+         'OA other gold 2018 WoS.csv']
+
+## Create new renamed files in utf-8 encoding with summaries removed
+newFiles = []
+for i, f in enumerate(files):
+    newFn = 'wos' + str(i) + '.csv'
+    newFiles.append( (newFn, f) )
+    fn = os.path.join('../data', f)
+    with open(fn, 'rt', encoding='windows-1251') as infile:
+        with open(newFn, 'wt') as outfile:
+            for l in infile.readlines():
+                if ',,,,,,,,' in l:
+                    # stop after separator between data and summary reached
+                    break
+                else:
+                    outfile.write(l)
+                    
+with open('fileOrigins.csv', 'wt') as manifest:
+    manifest.write('Old,New\n')
+    for a, b in newFiles:
+        manifest.write('"{0}",{1}\n'.format(b, a))
