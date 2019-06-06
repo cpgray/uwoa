dff46f5dbee03ef0128b85b121f467e21d4dd8f8
Christopher P Gray (cpgray) <cpgray@uwaterloo.ca>
2019-06-06 09:36:23 -0400

    added output of csv to reportGen

--- report/reportGen.py
+++ report/reportGen.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
 import bibextract
+import csv
 from pprint import pprint
 
 pubs = bibextract.getPubs()
 
 pprint(pubs)
+
+with open('pubsReport.csv', 'wt') as pubscsv:
+    wtr = csv.DictWriter(pubscsv, fieldnames=bibextract.fieldsOfInterest)
+    wtr.writeheader()
+    for row in pubs:
+        wtr.writerow(row)
+
