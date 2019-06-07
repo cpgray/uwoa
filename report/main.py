3af480c442db1d387336c1a72d0133e7733665f4
Christopher P Gray (cpgray) <cpgray@uwaterloo.ca>
2019-06-07 17:03:13 -0400

    worked on fetching Crossref data

--- report/main.py
+++ report/main.py
@@ -2,7 +2,11 @@
 
 import os
 import csv
+import json
+from urllib.request import urlopen
+from urllib.parse import quote
 from pprint import pprint
+from time import sleep
 
 ## list of files from Shannon with WoS data
 files = ['OA 2013-2017.csv',
@@ -13,6 +17,11 @@ files = ['OA 2013-2017.csv',
 fieldsOfInterest = ['Accession Number', 'DOI', 'Article Title', 'Authors',
                     'Source', 'Research Area', 'Publication Date',
                     'Times Cited']
+
+xrefURL = 'https://api.crossref.org/works/{0}?'
+xrefURL += 'mailto=libxrefclient@library.uwaterloo.ca'
+
+xrefFoI = ['publisher', 'is-referenced-by-count', 'container-title', 'subject']
 
 ## Create new renamed files in utf-8 encoding with summaries removed
 newFiles = []
@@ -60,3 +69,16 @@ with open('combined.csv', 'wt') as outfile:
         # change the list of filenames into a comma delimited string
         newRows[k]['Filename'] = ','.join(newRows[k]['Filename'])
         wtr.writerow(newRows[k])
+
+with open('combined.csv', 'rt') as infile:
+    rdr = csv.DictReader(infile)
+    for l in rdr:
+        doi = quote(l['DOI'])
+        resp = urlopen(xrefURL.format(doi))
+        xrefJSON = resp.read().decode('utf-8')
+        xrefResp = json.loads(xrefJSON)
+        if xrefResp['status'] == 'ok':
+            xrefData = xrefResp['message']
+            filtered = { k: xrefData.get(k) for k in xrefFoI }
+            print(json.dumps(filtered, indent=2))
+        sleep(5)
