4518eceb1d8fd553a76adb392932cc3d3170fdca
Christopher P Gray (cpgray) <cpgray@uwaterloo.ca>
2019-06-07 11:55:29 -0400

    updated README.MD and added code to filter and combine data from all 4 CSV files

--- report/main.py
+++ report/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import csv
 from pprint import pprint
 
 ## list of files from Shannon with WoS data
@@ -8,6 +9,10 @@ files = ['OA 2013-2017.csv',
          'OA 2018 WoS.csv',
          'OA other 2013-2017.csv',
          'OA other gold 2018 WoS.csv']
+
+fieldsOfInterest = ['Accession Number', 'DOI', 'Article Title', 'Authors',
+                    'Source', 'Research Area', 'Publication Date',
+                    'Times Cited']
 
 ## Create new renamed files in utf-8 encoding with summaries removed
 newFiles = []
@@ -23,8 +28,33 @@ for i, f in enumerate(files):
                     break
                 else:
                     outfile.write(l)
-                    
+
+## record manifest of new files and their origin files
 with open('fileOrigins.csv', 'wt') as manifest:
     manifest.write('Old,New\n')
     for a, b in newFiles:
         manifest.write('"{0}",{1}\n'.format(b, a))
+
+## combine/filter: reduce fields, deduplicate, remove items without DOIs
+newRows = {}
+for n, o in newFiles:
+    with open(n, 'rt') as infile:
+        rdr = csv.DictReader(infile)
+        for row in rdr:
+            doi = row['DOI']
+            if doi == 'n/a':
+                continue
+            newRow = { k:row[k] for k in fieldsOfInterest }
+            if doi in newRows.keys():
+                newRows[doi]['Filename'].append(o)
+            else:
+                newRow['Filename'] = [o]
+                newRows[doi] = newRow
+
+with open('combined.csv', 'wt') as outfile:
+    wtr = csv.DictWriter(outfile,
+                         fieldnames = fieldsOfInterest + ['Filename'])
+    wtr.writeheader()
+    for k in newRows.keys():
+        newRows[k]['Filename'] = ','.join(newRows[k]['Filename'])
+        wtr.writerow(newRows[k])
