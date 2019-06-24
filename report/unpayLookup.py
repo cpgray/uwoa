09d9d83f89e6c0a13340b30d42ef91947d76db75
Christopher P Gray (cpgray) <cpgray@uwaterloo.ca>
2019-06-24 13:38:17 -0400

    finished script to get and merge in Unpaywall data

--- report/unpayLookup.py
+++ report/unpayLookup.py
@@ -5,29 +5,32 @@ import json
 from urllib.request import urlopen
 from urllib.parse import quote
 from urllib.error import HTTPError
-from collections import defaultdict
 
 unpayurl = 'https://api.unpaywall.org/v2/{0}?email=cpgrayca@uwaterloo.ca'
 
+xrefMergedFoI = ['DOI', 'Filename', 'Article Title', 'Authors', 'Source',
+             'Research Area', 'Publication Date', 'Times Cited', 'publisher',
+             'is-referenced-by-count', 'container-title', 'subject',
+             'crossref-data']
 unpayFoI = ['is_oa', 'journal_is_oa', 'oa_status']
+mergedFoI = xrefMergedFoI + unpayFoI
 
-unpayValues = defaultdict(set)
-
-with open('combined.csv', 'rt') as infile:
+with open('xrefMerged.csv', 'rt') as infile:
     rdr = csv.DictReader(infile)
-    for row in rdr:
-        doi = quote(row['DOI'])
-        try:
-            resp = urlopen(unpayurl.format(doi))
-        except HTTPError as e:
-            print(doi)
-            print(str(e))
-            continue
-        unpayJSON = resp.read().decode('utf-8')
-        unpayResp = json.loads(unpayJSON)
-        for k in unpayFoI:
-            unpayValues[k].add(unpayResp[k])
-    for k in unpayFoI:
-        print(k, unpayValues[k])
-    
+    with open('unpayAdded.csv', 'wt') as outfile:
+        wtr = csv.DictWriter(outfile, fieldnames=mergedFoI)
+        wtr.writeheader()
+        for row in rdr:
+            doi = quote(row['DOI'])
+            try:
+                resp = urlopen(unpayurl.format(doi))
+            except HTTPError as e:
+                for k in unpayFoI:
+                    row[k] = None
+                wtr.writerow(row)
+                continue
+            unpayJSON = resp.read().decode('utf-8')
+            unpayResp = json.loads(unpayJSON)
+            filtered = { key:unpayResp[key] for key in unpayFoI }
+            wtr.writerow({**row, **filtered})
 
